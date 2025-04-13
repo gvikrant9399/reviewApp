885ad5f19c216cea4636293bfd5c1152f08e34d0
gvikrant9399 <vikrantghode9399@gmail.com>
2025-04-13 18:39:15 +0100

    new navbar changes

--- routes/index.js
+++ routes/index.js
@@ -18,37 +18,32 @@ router.get('/api/transactions', async (req, res) => {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('index', { title: '', name:null });
 });
 
 router.post('/', function(req, res, next) {
 
-  res.render('index', { title: 'Welcome to Web3 BootStrap App', name:req.body.name });
+  res.render('index', { title: '', name:req.body.name });
 });
 
 router.get('/page1', function(req, res, next) {
-  res.render('page1', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page1', { title: '', name:null });
 });
 
 router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page2', { title: '', name:null });
 });
 
 router.get('/page3', function(req, res, next) {
-  res.render('page3', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page3', { title: '', name:null });
 });
 
 router.get('/page4', function(req, res, next) {
-  res.render('page4', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page4', { title: '', name:null });
 });
-
 
 router.get('/page5', function(req, res, next) {
-  res.render('page5', { title: 'Welcome to Web3 BootStrap App', name:null });
-});
-
-router.get('/page6', function(req, res, next) {
-  res.render('page6', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page5', { title: '', name:null });
 });
 
 
